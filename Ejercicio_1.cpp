53de609bbda6b24426d1206e7144b46d333ca057
LucasMeoli <meolilucas@gmail.com>
2021-12-13 10:28:07 -0300

    Se agrega el ejercicio 1

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int a;
+    char b;
+    float c;
+    double d;
+    bool e;
+
+
+    std::cout << "int =" << sizeof(a) << " char = " << sizeof(b) << std::endl;
+    std::cout << "float =" << sizeof(c) << " double = " << sizeof(d) << std::endl;
+    std::cout << "bool =" << sizeof(e) << std::endl;
+    return 0;
+}
+
+
